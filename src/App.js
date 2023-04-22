6e45dd102cd3d7db81710a13d8ec3cb541bf881e
Tech-Kat <katpelle@pursuit.org>
2023-04-21 23:35:36 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Facts from './Pages/Facts';
+import Resources from './Pages/Resources';
+import Survey from './Pages/Survey';
+import Four0Four from './Pages/Four0Four';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/facts" element={<Facts />} />
+            <Route path="/resources" element={<Resources/>}/>
+            <Route path="/survey" element={<Survey />} />
+            <Route path="*" element={<Four0Four />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
